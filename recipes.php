b835766af911e4b32348078ab019ced4f010b8e4
beyoudesign <cjohns48@lesley.edu>
2022-10-22 19:11:39 -0400

    changed everything to php

---
+++ recipes.php
@@ -0,0 +1,35 @@
+<?php
+        
+    ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+
+<title>Chelsea Johnson and Lucie Ortega</title>
+
+<head>
+    <div id = "header">
+        <h1>Recipe of the Day</h1>
+    </div>
+</head>
+
+
+
+<body>
+
+<?php
+        include "navigation.php";
+        include "footer.php";
+    
+    ?>
+
+
+   
+
+
+</body>
+</html>
