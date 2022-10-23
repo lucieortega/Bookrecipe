d4d71afc59ff510b7b4b564001f485d97582a88e
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-10-22 21:01:10 -0400

    added a contact us box
    Signed-off-by: lucieortega <102424066+lucieortega@users.noreply.github.com>

--- join_us.php
+++ join_us.php
@@ -3,7 +3,7 @@
 
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 
 <link rel="stylesheet" type="text/css" href="style.css"/>
 
@@ -14,14 +14,6 @@
     <div id = "header">
         <h1>Join the Community</h1>
     </div>
-
-
-
-
-</head>
-
-
-
 <body>
 
 <?php
@@ -29,8 +21,42 @@
         include "footer.php";
     
     ?>
-   
-
-
 </body>
+ </head>
+
+<body>
+   <a href="join_us.php">
+       <img src="foodlogo.jpeg" width="200px" height="200px" alt="logofood"/>
+       </a>
+   <div class="inner contact">
+<!-- Form Area -->
+<div class="contact-form">
+<!-- Form -->
+<form id="contact-us" method="post" action="#">
+<!-- Left Inputs -->
+<div class="col-xs-6 wow animated slideInLeft" data-wow-delay=".5s">
+<!-- Name -->
+<input type="text" name="name" id="name" required="required" class="form" placeholder="Name" />
+<!-- Email -->
+<input type="email" name="mail" id="mail" required="required" class="form" placeholder="Email" />
+<!-- Subject -->
+<input type="text" name="subject" id="subject" required="required" class="form" placeholder="Subject" />
+</div><!-- End Left Inputs -->
+<!-- Right Inputs -->
+<div class="col-xs-6 wow animated slideInRight" data-wow-delay=".5s">
+<!-- Message -->
+<textarea name="message" id="message" class="form textarea"  placeholder="Message"></textarea>
+</div>
+<!-- End Right Inputs -->
+<!-- Send Button -->
+<button type="submit" id="submit" name="submit" class="form-btn semibold" href="mailto:lucie.ortega@live.fr">Send Message</button> 
+</div><!-- End Bottom Submit -->
+<!-- Clear -->
+<div class="clear"></div>
+</form>
+</div><!-- End Contact Form Area -->
+</div><!-- End Inner -->
+
+
+</head>
 </html>
