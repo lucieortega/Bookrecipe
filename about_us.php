e8ed77511d3ae66aea2c769f821165c46819ecc5
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-10-22 21:38:38 -0400

    added paragraph on style sheet
    Signed-off-by: lucieortega <102424066+lucieortega@users.noreply.github.com>

--- about_us.php
+++ about_us.php
@@ -19,21 +19,16 @@
 
 </head>
 
-<h3>With a love story</h3>
-            
+<h3>With a love story</h3>       
 <body>
-   
 <?php
         include "navigation.php";
         include "footer.php";
     
     ?>
-
 </body>
-<div class="imgContainer">
-        <!-- <div class="img-with-text"> -->
             <img class="ramen" src="ramen.jpeg" style='float: left;' alt="ramen bowl"/>
-            </div>
+           
 <p> We all have that one dish that excites our palates and makes us pine for those special memories 
        with Mom, Dad, Grandma or maybe a favorite aunt who created these delicious, comforting dishes. 
        Every family has that favorite dish — and there's a story behind each recipe.
