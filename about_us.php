cfe5af47fe6604f954464f67a7b07f4938f81066
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-10-22 21:36:30 -0400

    fix some pb
    Signed-off-by: lucieortega <102424066+lucieortega@users.noreply.github.com>

--- about_us.php
+++ about_us.php
@@ -21,8 +21,6 @@
 
 <h3>With a love story</h3>
             
-
-            <img class="ramen" src="ramen.jpeg" style='float: left;' alt="ramen bowl"/>
 <body>
    
 <?php
@@ -32,6 +30,10 @@
     ?>
 
 </body>
+<div class="imgContainer">
+        <!-- <div class="img-with-text"> -->
+            <img class="ramen" src="ramen.jpeg" style='float: left;' alt="ramen bowl"/>
+            </div>
 <p> We all have that one dish that excites our palates and makes us pine for those special memories 
        with Mom, Dad, Grandma or maybe a favorite aunt who created these delicious, comforting dishes. 
        Every family has that favorite dish — and there's a story behind each recipe.
