3954af91393181b6854ec391569241b0a48f8e1d
beyoudesign <cjohns48@lesley.edu>
2022-10-25 14:47:41 -0400

    updated the database name

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,10 @@
-<php
-    $dbServer = "localhost";
-    $dbUsername = "root";    // This is the same username as phpMyAdmin
-    $dbPassword = "root";    // This is the same password as phpMyAdmin
-    $dbName = "blog_cms";
+<?php
 
-    // Create connection
-    $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+$dbServername = "localhost";
+$dbUsername = "root"; //This is the same username as phpMyAdmin
+$dbPassword = "root"; //This is the same password as phpMyAdmin
+$dbName = "bookrecipe";
+
+//Create connection
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
