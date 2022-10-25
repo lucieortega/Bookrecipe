e4ba3763e24051a47aef75827df80e743081ba75
lucieortega <102424066+lucieortega@users.noreply.github.com>
2022-10-25 17:32:43 -0400

    changed name
    Signed-off-by: lucieortega <102424066+lucieortega@users.noreply.github.com>

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
     $dbServer = "localhost";
     $dbUsername = "root";    // This is the same username as phpMyAdmin
     $dbPassword = "root";    // This is the same password as phpMyAdmin
-    $dbName = "blog_cms";
+    $dbName = "Bookrecipe";
 
     // Create connection
     $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
