2df85260f1068c1243df3dd859056ad63fb83922
beyoudesign <cjohns48@lesley.edu>
2022-10-28 21:43:26 -0400

    still working on getting data from db

--- db_connect.php
+++ db_connect.php
@@ -4,12 +4,26 @@
 $dbServername = "localhost";
 $dbUsername = "root"; //This is the same username as phpMyAdmin
 $dbPassword = "root"; //This is the same password as phpMyAdmin
-$dbName = "Bookrecipe";
+$dbName = "book_recipe";
 
 //Create connection
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
-  ?>
+}
+
+// $sql = "SELECT id, title, nameofdish, FROM Recipes";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     // output data of each row
+// while($row = $result->fetch_assoc()) {
+//     echo "id: " . $row["id"]. "title: " . $row["nameofdish"]. "<br>";
+// }
+// }   else {
+//     echo "0 results";
+// }
+// $conn->close();
+?>
+
--- recipes.php
+++ recipes.php
@@ -1,15 +1,3 @@
-<?php
- include_once 'db_connect.php'; 
-
-
- $sql = "SELECT * FROM Recipes";
- $result = mysqli_query($conn, $sql);
-
- 
-
-?>
- 
-
 <!DOCTYPE html>
 
 <html>
@@ -31,14 +19,48 @@
 <body>
 
 <?php
+
+ 
+
+function getRecipeDetailsFromDatabase() {
+    include_once 'db_connect.php';
+    $sql = 'SELECT id, title, name_of_dish FROM Recipes';
+    mysql_select_db('book_recipe');
+    $retval = mysql_query( $sql, $conn ); 
+
+if(! $retval ) {
+   die('Could not get data: ' . mysql_error());
+}
+
+while($row = mysql_fetch_assoc($retval)) {
+   echo "EMP ID :{$row['emp_id']}  <br> ".
+      "EMP NAME : {$row['emp_name']} <br> ".
+      "EMP SALARY : {$row['emp_salary']} <br> ".
+      "--------------------------------<br>";
+}
+
+echo "Fetched data successfully\n";
+
+mysql_close($conn); 
+
+
+?>
+
+
+ 
+
+
+<?php
         include "navigation.php";
         include "footer.php";
     
     ?>
 
 
+
+
    
 
 
 </body>
-</html>
+</html> 
